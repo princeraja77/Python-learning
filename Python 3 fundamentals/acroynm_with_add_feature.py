430083c52e7800e6b412aefeeb9d011b467323a9
princeraja77 <138289599+princeraja77@users.noreply.github.com>
2024-10-17 04:26:02 +0000

    acronym with lookup and add feature

---
+++ Python 3 fundamentals/acroynm_with_add_feature.py
@@ -0,0 +1,35 @@
+import acroynym_look_up as al
+
+def add_acronym(acronym):
+    res=al.look_up(acronym)
+    if res==False:
+        definition=input("Enter the definition of acronym")
+        with open("/workspaces/Python-learning/Python 3 fundamentals/acroynm.txt",'a') as fo:
+            fo.write(f"\n{acronym} -> {definition}.")
+    else:
+        print(f"{acronym} definition already exists")
+
+def main():
+    while True:
+        print("Welcome to acronym finder:")
+        print("Enter 1 to look for acronym")
+        print("Enter 2 to add an acronym")
+        print("Enter 0 to exit the program")
+        choice=int(input("Enter your choice: "))
+        if choice==1:
+            acronym=input("Enter the acronym you want to search\n")
+            res=al.look_up(acronym)
+            if res==False:
+                print(f"{acronym} acroynm not found")
+            else:
+                print(res)
+        elif choice==2:
+            acronym=input("Enter the acronym you want to add\n")
+            add_acronym(acronym)
+        elif choice==0:
+            break
+        else:
+            print("Invalid Input")
+
+if __name__=="__main__":
+    main()
--- Python 3 fundamentals/acroynym_look_up.py
+++ Python 3 fundamentals/acroynym_look_up.py
@@ -1,10 +1,15 @@
-srh_word=input("Enter the acronym you want definition:\n")
-found=False
-with open("/workspaces/Python-learning/Python 3 fundamentals/acroynm.txt") as fo:
-    for line in fo:
-        if srh_word in line:
-            print(line)
-            found=True
-            break
-if not found:
-    print(f"{srh_word} acroynm not found")
+def look_up(srh_word):
+    with open("/workspaces/Python-learning/Python 3 fundamentals/acroynm.txt") as fo:
+        for line in fo:
+            if srh_word in line:
+                return line
+        return False
+
+if __name__=="__main__":
+    srh_word=input("Enter the acronym you want definition:\n")
+    res=look_up(srh_word)
+    if res==False:
+        print(f"{srh_word} acroynm not found")
+    else:
+        print(res)
+
